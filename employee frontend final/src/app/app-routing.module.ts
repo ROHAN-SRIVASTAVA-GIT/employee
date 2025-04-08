815ff0b3ddfaf6f2abe31060d5fca18135754b37
Rohan Srivastava <rohansrivastava3125@gmail.com>
2025-04-08 12:11:34 +0530

    Initial commit

---
+++ employee frontend final/src/app/app-routing.module.ts
@@ -0,0 +1,54 @@
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+// import { EmployeeListComponent } from './employee-list/employee-list.component';
+// import { AddEmployeeComponent } from './add-employee/add-employee.component';
+// import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+// import { ShowDetailsComponent } from './show-details/show-details.component';
+// import { HomeComponent } from './home/home.component';
+// import { AdminLoginComponent } from './admin-login/admin-login.component';
+
+
+
+// const routes: Routes = [
+
+//   {path:"show-all-employees",component: EmployeeListComponent},
+//   {path:"add-employee", component: AddEmployeeComponent},
+//   {path:'', redirectTo: "home", pathMatch:"full"},
+//   {path:'updating-by-id/:id',component:UpdateEmployeeComponent},
+//   {path:'details-of-employee/:id',component:ShowDetailsComponent},
+//   {path:'home',component:HomeComponent},
+//   {path:'login',component:AdminLoginComponent}
+// ];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { UpdateEmployeeComponent } from './update-employee/update-employee.component';
+import { ShowDetailsComponent } from './show-details/show-details.component';
+// import { HomeComponent } from './home/home.component'; // 👈 removed or commented
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+
+const routes: Routes = [
+  { path: 'show-all-employees', component: EmployeeListComponent },
+  { path: 'add-employee', component: AddEmployeeComponent },
+  { path: '', redirectTo: 'show-all-employees', pathMatch: 'full' }, // 👈 updated default route
+  { path: 'updating-by-id/:id', component: UpdateEmployeeComponent },
+  { path: 'details-of-employee/:id', component: ShowDetailsComponent },
+  // { path: 'home', component: HomeComponent }, // 👈 disabled
+  { path: 'login', component: AdminLoginComponent },
+  { path: '**', redirectTo: 'show-all-employees' } // 👈 optional: catch-all fallback
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
